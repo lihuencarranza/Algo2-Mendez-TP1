2e9d751bfd33fb45661ff7d72dbd5a85d95be821
Lihuen Carranza <lcarranza@fi.uba.ar>
2022-03-27 13:46:22 -0300

    eliminé acrhivos movidos a src

--- interaccion.c
+++
@@ -1,8 +0,0 @@
-#include "estructuras.h"
-#include "interaccion.h"
-#include <stdlib.h>
-
-struct interaccion *interaccion_crear_desde_string(const char *string)
-{
-	return NULL;
-}
--- objeto.c
+++
@@ -1,8 +0,0 @@
-#include "objeto.h"
-#include "estructuras.h"
-#include <stdlib.h>
-
-struct objeto *objeto_crear_desde_string(const char *string)
-{
-	return NULL;
-}
